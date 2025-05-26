3c0366fc0e667b7da572ee648ccaf4df004726bd
EugeneKulagin <kulagines@gmail.com>
2025-05-26 19:58:47 +0300

    Kata

---
+++ src/main/java/DAO/UserDaoJDBClmpl.java
@@ -0,0 +1,35 @@
+//package DAO;
+//
+//import Model.User;
+//
+//import java.util.List;
+//
+//public class UserDaoJDBClmpl implements UserDao {
+//    public UserDaoJDBClmpl() {
+//
+//    }
+//
+//    public void createUsersTable() {
+//
+//    }
+//
+//    public void dropUsersTable() {
+//
+//    }
+//
+//    public void saveUser(String name, String lastName, byte age) {
+//
+//    }
+//
+//    public void removeUserById(Long id) {
+//
+//    }
+//
+//    public List<User> getAllUsers() {
+//        return null;
+//    }
+//
+//    public void cleanUsersTable() {
+//
+//    }
+//}
